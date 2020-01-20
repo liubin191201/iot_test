f47c9b8f6e3643c00df2362617dc560b374842ae
liubin <liubin191201@gmail.com>
2020-01-20 22:15:58 +0800

    feat: inti

---
+++ netty/src/main/java/com/bin/liu/iot/NettyApp.java
@@ -0,0 +1,17 @@
+package com.bin.liu.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class NettyApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(NettyApp.class, args);
+    }
+}
